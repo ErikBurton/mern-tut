a27142e457e79ff0fa758b3563bfe45b8ec412c6
Erik Burton <erikburton1@gmail.com>
2022-02-16 19:45:20 -0500

    Adds error handling

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,32 +1,38 @@
+const asyncHandler = require('express-async-handler')
+
 // @description Get goals
 // @route       GET /api/goals
 // @access      PRIVATE
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get goals' })
-}
+})
 
 // @description Set goal
 // @route       POST /api/goals
 // @access      PRIVATE
-const setGoal = (req, res) => {
-    console.log(req.body)
+const setGoal = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text field ')
+    }
 
     res.status(200).json({ message: 'Set goal' })
-}
+})
 
 // @description UPDATE goal
 // @route       PUT /api/goals/:id
 // @access      PRIVATE
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update goal ${req.params.id}` })
-}
+})
 
 // @description Delete goal 
 // @route       DELETE /api/goals/:ID
 // @access      PRIVATE
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete goal ${req.params.id}` })
-}
+})
+
 module.exports = {
     getGoals,
     setGoal,
