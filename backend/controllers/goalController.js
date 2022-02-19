5d51a6f460ca5f04c47c70e4874b0fbbbbf5dcb4
Erik Burton <erikburton1@gmail.com>
2022-02-18 20:01:40 -0500

    Complets CRUD functionality

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -31,14 +31,37 @@ const setGoal = asyncHandler(async (req, res) => {
 // @route       PUT /api/goals/:id
 // @access      PRIVATE
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}` })
+
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updateGoal)
 })
 
 // @description Delete goal 
 // @route       DELETE /api/goals/:ID
 // @access      PRIVATE
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}` })
+
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove()
+
+
+    res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
