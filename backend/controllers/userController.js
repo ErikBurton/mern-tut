9892634506a0bbacac20c6163cf0fb629bba14c6
Erik Burton <erikburton1@gmail.com>
2022-02-19 17:44:25 -0500

    User Routes & controller funtions

---
+++ backend/controllers/userController.js
@@ -0,0 +1,26 @@
+// @description Register new user
+// @route       POST /api/users
+// @access      PUBLIC
+const registerUser = (req, res) => {
+    res.json({ message: 'Register User' })
+}
+
+// @description Authenticate a user
+// @route       POST /api/users/login
+// @access      PUBLIC
+const loginUser = (req, res) => {
+    res.json({ message: 'Login User' })
+}
+
+// @description Get user data
+// @route       GET /api/users/me
+// @access      PUBLIC
+const getMe = (req, res) => {
+    res.json({ message: 'User data display' })
+}
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe,
+}
