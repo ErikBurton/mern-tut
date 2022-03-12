48110a97df5967b0d633a61928527dee0e3433f6
Erik Burton <erikburton1@gmail.com>
2022-03-12 13:47:29 -0500

    Adds onSubmit function

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -20,6 +20,10 @@ function Register() {
     }))
   }
 
+  const onSubmit = (e) => {
+      e.prevendDefault()
+  }
+
   
   return (
     <>
@@ -31,55 +35,57 @@ function Register() {
       </section>
 
       <section className='form'>
-          <div className='form-group'>
-            <input
-              type='text'
-              className='form-control'
-              id='name'
-              name='name'
-              value={name}
-              placeholder='Enter your name'
-              onChange={onChange}
-            />
-          </div>
-          <div className='form-group'>
-            <input
-              type='email'
-              className='form-control'
-              id='email'
-              name='email'
-              value={email}
-              placeholder='Enter your email'
-              onChange={onChange}
-            />
-          </div>
-          <div className='form-group'>
-            <input
-              type='password'
-              className='form-control'
-              id='password'
-              name='password'
-              value={password}
-              placeholder='Enter password'
-              onChange={onChange}
-            />
-          </div>
-          <div className='form-group'>
-            <input
-              type='password'
-              className='form-control'
-              id='password2'
-              name='password2'
-              value={password2}
-              placeholder='Confirm password'
-              onChange={onChange}
-            />
-          </div>
-          <div className='form-group'>
-            <button type='submit' className='btn btn-block'>
-              Submit
-            </button>
-          </div>
+          <form onSubmit={onSubmit}>
+            <div className='form-group'>
+                <input
+                type='text'
+                className='form-control'
+                id='name'
+                name='name'
+                value={name}
+                placeholder='Enter your name'
+                onChange={onChange}
+                />
+            </div>
+            <div className='form-group'>
+                <input
+                type='email'
+                className='form-control'
+                id='email'
+                name='email'
+                value={email}
+                placeholder='Enter your email'
+                onChange={onChange}
+                />
+            </div>
+            <div className='form-group'>
+                <input
+                type='password'
+                className='form-control'
+                id='password'
+                name='password'
+                value={password}
+                placeholder='Enter password'
+                onChange={onChange}
+                />
+            </div>
+            <div className='form-group'>
+                <input
+                type='password'
+                className='form-control'
+                id='password2'
+                name='password2'
+                value={password2}
+                placeholder='Confirm password'
+                onChange={onChange}
+                />
+            </div>
+            <div className='form-group'>
+                <button type='submit' className='btn btn-block'>
+                Submit
+                </button>
+            </div>
+        </form>
       </section>
     </>
   )
